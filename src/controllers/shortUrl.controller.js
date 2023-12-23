606eea7cee66aabf67c0dfb3dbe19631e8afa6ea
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-12-23 08:05:32 +0530

    fixes some issue

--- src/controllers/shortUrl.controller.js
+++ src/controllers/shortUrl.controller.js
@@ -1,72 +1,63 @@
-
 import shortid from "shortid";
+import validator from "validator";
 import { Url } from "../models/url.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const shortUrl = async (req, res) => {
-  try {
-    const { originalUrl } = req.body;
+const shortUrl = asyncHandler(async (req, res) => {
+  const { originalUrl } = req.body;
 
-    if (!originalUrl) {
-      throw new ApiError(400, "Original url is required");
-    }
-
-
-    const isUrlExist = await Url.findOne({ originalUrl });
-
-    // if url already exist
-    if (isUrlExist) {
-      return res
-        .status(200)
-        .json(
-          new ApiResponse(200, { originalUrl, shortUrl: isUrlExist.shortUrl })
-        );
-    }
-
-
-    // if url doest not exist
-
-    const shortUrl = shortid.generate();
-    const url = new Url({ originalUrl, shortUrl });
-    await url.save();
-    return res
-      .status(201)
-      .json(
-        new ApiResponse(
-          200,
-          { originalUrl, shortUrl },
-          "short url created successfully"
-        )
-      );
-  } catch (error) {
-    throw new ApiError(500, "Internal server error");
+  if (!originalUrl) {
+    throw new ApiError(400, "Original url is required");
   }
-};
 
+  if (!validator.isURL(originalUrl)) {
+    throw new ApiError(400, "Invalid url");
+  }
 
+  const isUrlExist = await Url.findOne({ originalUrl });
 
-const getOriginalUrl = async(req, res)=> {
-    try {
-        const {shortUrl} = req.params;
-        
-        if(!shortUrl) {
-            throw new ApiError(400, 'shortUrl is required');
-        }
-        
-        const url = await Url.findOne({shortUrl});
+  // if url already exist
+  if (isUrlExist) {
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { originalUrl, shortUrl: isUrlExist.shortUrl })
+      );
+  }
 
-        if(!url) {
-            throw new ApiError(404, 'Url not found');
-        }
-         
-        res.status(200)
-        .json(new ApiResponse(200, url.originalUrl));
-        
-    } catch (error) {
-        throw new ApiError(500, 'Internal server error');
-    }
-   
-}
+  // if url doest not exist
+
+  const shortUrl = shortid.generate();
+
+  const url = new Url({ originalUrl, shortUrl });
+  await url.save();
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        200,
+        { originalUrl, shortUrl },
+        "short url created successfully"
+      )
+    );
+});
+
+const getOriginalUrl = asyncHandler(async (req, res) => {
+  const { shortUrl } = req.params;
+
+  if (!shortUrl) {
+    throw new ApiError(400, "shortUrl is required");
+  }
+
+  const url = await Url.findOne({ shortUrl });
+
+  if (!url) {
+    throw new ApiError(404, "Url not found");
+  }
+
+  res.redirect(url.originalUrl);
+});
 
 export { shortUrl, getOriginalUrl };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,73 +1,61 @@
-import e from 'express';
-import {User} from '../models/user.model.js';
-import {ApiError} from '../utils/ApiError.js';
-import {ApiResponse} from '../utils/ApiResponse.js';
-import {generateAccessToken} from '../utils/generateAccessToken.js';
+import e from "express";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { generateAccessToken } from "../utils/generateAccessToken.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
 
+  if ([username, email, password].some((field) => field.trim() == "")) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-const registerUser = async(req, res)=> {
-    try {
-        const {username, email, password} = req.body;
+  const isUserExist = await User.findOne({ $or: [{ username }, { email }] });
 
-        if([username, email, password].some((field)=> field.trim() == "")) {
-            throw new ApiError(400, 'All fields are required');
-        }
+  if (isUserExist) {
+    throw new ApiError(409, "User already exist");
+  }
 
-        const isUserExist = await User.findOne({$or:[{username},{email}]});
+  const user = await User.create({ username, email, password });
+  const registerUser = await User.findById(user._id).select("-password");
 
-        if(isUserExist) {
-            throw new ApiError(409, 'User already exist');
-        }
-        
-    
-       const user = await User.create({username, email, password});
-       const registerUser = await User.findById(user._id).select('-password');
+  return res
+    .status(201)
+    .json(new ApiResponse(200, registerUser, "User register successfully"));
+});
 
-        return res.status(201)
-        .json(new ApiResponse(200, registerUser, 'User register successfully'));
-    } catch (error) {
-        throw new ApiError(500, 'Internal server error');
-    }
-}
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email) {
+    throw new ApiError(400, "username or email is required");
+  }
 
+  const user = await User.findOne({ $or: [{ email }, { username }] });
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
 
-const loginUser = async(req, res)=> {
-     try {
-        const {username, email, password} = req.body;
-        if(!username || !email) {
-            throw new ApiError(400, 'username or email is required');
-        }
-    
-        const user = await User.findOne({$or:[{email}, {username}]});
-        if(!user) {
-            throw new ApiError(404, 'User does not exist');
-        }
-         
-        // cpmpare password
-        const isPasswordCorrect = await user.isPasswordCorrect(password);
-        if(!isPasswordCorrect) {
-            throw new ApiError(401, 'Invalid user credential');
-        }
+  // cpmpare password
+  const isPasswordCorrect = await user.isPasswordCorrect(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Invalid user credential");
+  }
 
+  const loginUser = await User.findById(user._id).select("-password");
+  // create accessToken
+  const accessToken = await generateAccessToken(user._id);
 
-        const loginUser = await User.findById(user._id).select('-password');
-        // create accessToken
-        const accessToken = await generateAccessToken(user._id);
-    
-        return res.status(200)
-        .json(new ApiResponse(200, {loginUser, accessToken}, 'User login successfully'));
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { loginUser, accessToken },
+        "User login successfully"
+      )
+    );
+});
 
-     } catch (error) {
-        throw new ApiError(500, 'Internal server error');
-     }
-}
-
-
-
-
-
-export {
-    registerUser,
-    loginUser
-}
+export { registerUser, loginUser };
